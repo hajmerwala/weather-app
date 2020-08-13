78bd86464923b1b6d68e761cfd6867d63c8ca853
Hussain Ajmerwala <60471016+hajmerwala@users.noreply.github.com>
2020-08-13 18:32:07 +0530

    Changes for geolocation access token

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiaGFqbWVyd2FsYSIsImEiOiJja2RzdDFncW0waTFiMnJxc3hhemE4djJ5In0.cxTxZqNExzKfywZ2_cnImg&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
